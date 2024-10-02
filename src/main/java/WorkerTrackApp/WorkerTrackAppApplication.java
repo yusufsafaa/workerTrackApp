f6ce922fe10992737ed306f1fc6edd99a1ee37c1
Yusuf Safa Koksal <yusuf68815@gmail.com>
2024-10-03 00:05:06 +0300

    first commit

---
+++ src/main/java/WorkerTrackApp/WorkerTrackAppApplication.java
@@ -0,0 +1,13 @@
+package WorkerTrackApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkerTrackAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkerTrackAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/WorkerTrackApp/WorkerTrackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package WorkerTrackApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkerTrackAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
